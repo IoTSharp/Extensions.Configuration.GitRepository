d35b5dcb406b00dd1c78bb7e5fc3f599aff90829
maikebing <mysticboy@live.com>
2025-04-01 19:28:08 +0800

    加入.Net 8中， 使用Startup 加载参数的demo

---
+++ DemoUseStartup/Program.cs
@@ -0,0 +1,32 @@
+namespace DemoUseStartup
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                 .ConfigureAppConfiguration((context, builder) =>
+                 {
+                     
+                    var pat= builder.AddUserSecrets("personal_access_tokens").Build();
+                    
+                     builder.AddGitRepository(cfg =>
+                                            cfg.WithHostUrl("https://gitlab.com/")
+                                                .WithRepositoryPath("IoTSharp/gitlabcfg")
+                                                .WithAuthenticationToken(pat.GetValue<string>("personal_access_tokens"))
+                                                .WithFileName($"{Environment.GetEnvironmentVariable("UIXEID")}/cfg.json")
+                                                .WithCache($"{context.HostingEnvironment.ContentRootPath}{System.IO.Path.DirectorySeparatorChar}appsettings.{context.HostingEnvironment.EnvironmentName}.json")
+                                            );
+                 })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
---
+++ DemoUseStartup/Startup.cs
@@ -0,0 +1,44 @@
+ 
+namespace DemoUseStartup
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Console.WriteLine($"abc={Configuration.GetValue<string>("abc")}");
+            // Add services to the container.
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+   
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllers();
+            });
+            app.UseStaticFiles();
+        }
+
+    }
+}
